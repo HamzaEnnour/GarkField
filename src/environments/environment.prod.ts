cff745b627912c9c961344f60519c216b650da3c
HamzaEnnour <49129326+HamzaEnnour@users.noreply.github.com>
2022-01-02 15:30:00 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,31 +2,9 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+//private
 export const environment = {
   production: true,
   SCARF_ANALYTICS : false,
-  adminRoot: '/football/app',
-  apiUrl:  'https://api.gark.app/api',
-  backend:  'https://api.gark.app/',
-  defaultMenuType: 'menu-default',
-  subHiddenBreakpoint: 1440,
-  menuHiddenBreakpoint: 768,
-  themeColorStorageKey: 'gark-themecolor',  
-  defaultColor: 'light.greenlime',
-  isDarkSwitchActive: true,
-  defaultDirection: 'ltr',
-  themeRadiusStorageKey: 'gark-themeradius',
-  isAuthGuardActive: false,
-  remember: "SYS_SEC",
-  rememberSecret: "eedaipiichah1Ahv4ug0oiriel6oop6rae2vodopoohaiphoghu9quaengaedi6g",
-  firebase: {
-    apiKey: "AIzaSyAW5XI1xtyDSCwXqgJxKY_bzLTJEHISoxo",
-    authDomain: "gark-b5d29.firebaseapp.com",
-    projectId: "gark-b5d29",
-    storageBucket: "gark-b5d29.appspot.com",
-    messagingSenderId: "482874847853",
-    appId: "1:482874847853:web:047579139ad926d0b78564",
-    measurementId: "G-VQXZB5PNJ6",
-    token: "AAAAcG2Vcm0:APA91bFVv7L8zlDq135rrc4mFdGpa4V-1KeTEIRIAEM7eH1AqK1caCiICRHuZmzve7OjyJILNyPPHCa2QuHPLryXEXH5Ohzt4vgqaZyY6V9MDODeilC-LntehcIvFA1xi-abkE3P74kI"
-  }
+
 };
